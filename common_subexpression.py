1185dfaa760560ac1c052a4e590c56cfd6bd03ff
Sanj-1873 <sanjnac@gmail.com>
2021-04-22 20:18:57 +0530

    buggy

--- common_subexpression.py
+++ common_subexpression.py
@@ -1,7 +1,7 @@
 f = open("TAC.txt","r")
 opt = open("common_sub.txt", "w")
 list_of_lines = f.readlines()
-
+re_written_res="temp"
 hold = {}
 for i in list_of_lines:
     if "function"  in i:
@@ -9,16 +9,24 @@ for i in list_of_lines:
     else:
         i = i.strip("\n")
         op,arg1,arg2,res = i.split()
-        result = op + arg1 + arg2 
+        result = op + arg1 + arg2
+        
         if result in hold:
             pointer = hold.get(result)
-            print(pointer)
+            re_written_res = res
             write_back = op+" "+arg1+" "+arg2+" "+pointer+"\n"
             opt.write(write_back)
         else:
             hold[result] = res
             write_back = op+" "+arg1+" "+arg2+" "+res+"\n"
             opt.write(write_back)
-
-
         
+        for j in  hold.items():
+            if re_written_res in j:
+                write_back = op+" "+re_written_res+" "+arg2+" "+res+"\n"
+                print(write_back)
+                opt.write(write_back)
+
+print(hold,"\n")
+print(pointer,"\n")
+print(re_written_res)
