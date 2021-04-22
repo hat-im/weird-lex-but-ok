fb91d44bafc3b5c4e4c6edf58616853b65ccad17
Sanj-1873 <sanjnac@gmail.com>
2021-04-22 16:42:39 +0530

    new lex and yacc

---
+++ IGC/sample.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int foo(int x, int y) {
+    int z = x +y;
+    return z; 
+}
+
+int main() {
+   int a = 5;
+   int b = 6;
+   b = a + 78;
+   for(a = 10; a <= 5; a++) {
+       b = 23;
+       if(a<b) {
+           a =10;
+           if(b<10) {
+               b=5;
+               int test = 0;
+           }
+           else {
+               int m=5;
+           }
+       }
+   }
+   char c = '2';
+}
+
